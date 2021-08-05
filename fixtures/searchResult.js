4759f9eec71bdf6c6bb2d5c9e461f857f320564f
Diana Eastman <deastman@mitre.org>
2021-08-04 21:31:21 -0400

    Add fixtures and required packages (were erroneously not included in the previous commit).

---
+++ fixtures/searchResult.js
@@ -0,0 +1,218 @@
+
+const searchResult = {
+    "id": "NGC-ahrqngcprivate-org-summaries-summary-46179-symptom-management-in-patients-with-lung-cancer-diagnosis-and-management-of-lung-cancer-3rd-ed-american-college-of-chest-physicians-evidencebased-clinical-practice-guidelines",
+    "text": {
+        "status": "generated",
+        "div": "<div></div>"
+    },
+    "url": "http://localhost:4567/fhir/Citation/NGC-ahrqngcprivate-org-summaries-summary-46179-symptom-management-in-patients-with-lung-cancer-diagnosis-and-management-of-lung-cancer-3rd-ed-american-college-of-chest-physicians-evidencebased-clinical-practice-guidelines",
+    "identifier": [
+        {
+            "system": "http://ahrq.gov/cedar",
+            "value": "NGC-ahrqngcprivate-org-summaries-summary-46179-symptom-management-in-patients-with-lung-cancer-diagnosis-and-management-of-lung-cancer-3rd-ed-american-college-of-chest-physicians-evidencebased-clinical-practice-guidelines"
+        }
+    ],
+    "title": "Symptom management in patients with lung cancer: diagnosis and management of lung cancer, 3rd ed: American College of Chest Physicians evidence-based clinical practice guidelines.",
+    "status": "active",
+    "date": "2021-07-22",
+    "publisher": "CEDAR",
+    "contact": [
+        {
+            "name": "CEDAR",
+            "telecom": [
+                {
+                    "system": "url",
+                    "value": "http://ahrq.gov/cedar",
+                    "use": "work"
+                }
+            ]
+        }
+    ],
+    "citedArtifact": {
+        "identifier": [
+            {
+                "system": "https://ahrqngcprivate.org",
+                "value": "009887"
+            }
+        ],
+        "dateAccessed": "2021-07-22",
+        "currentState": [
+            {
+                "coding": [
+                    {
+                        "system": "http://hl7.org/fhir/publication-status",
+                        "code": "active"
+                    }
+                ]
+            }
+        ],
+        "title": [
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/title-type",
+                            "code": "primary-human-use",
+                            "display": "Primary human use"
+                        }
+                    ]
+                },
+                "language": {
+                    "coding": [
+                        {
+                            "system": "urn:ietf:bcp:47",
+                            "code": "en-US",
+                            "display": "English (United States)"
+                        }
+                    ]
+                },
+                "text": "Symptom management in patients with lung cancer: diagnosis and management of lung cancer, 3rd ed: American College of Chest Physicians evidence-based clinical practice guidelines."
+            }
+        ],
+        "abstract": [
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/cited-artifact-abstract-type",
+                            "code": "primary-human-use",
+                            "display": "Primary human use"
+                        }
+                    ]
+                },
+                "language": {
+                    "coding": [
+                        {
+                            "system": "urn:ietf:bcp:47",
+                            "code": "en-US",
+                            "display": "English (United States)"
+                        }
+                    ]
+                },
+                "text": "Sample text"
+            }
+        ],
+        "publicationForm": [
+            {
+                "publishedIn": {
+                    "type": {
+                        "coding": [
+                            {
+                                "system": "http://terminology.hl7.org/CodeSystem/published-in-type",
+                                "code": "D019991",
+                                "display": "Database"
+                            }
+                        ]
+                    },
+                    "title": "NGC",
+                    "publisher": {
+                        "reference": "Organization/ngc",
+                        "display": "NGC"
+                    }
+                },
+                "articleDate": "2003-01-01",
+                "language": [
+                    {
+                        "coding": [
+                            {
+                                "system": "urn:ietf:bcp:47",
+                                "code": "en-US",
+                                "display": "English (United States)"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ],
+        "webLocation": [
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/article-url-type",
+                            "code": "full-text"
+                        }
+                    ]
+                },
+                "url": "https://ahrqngcprivate.org/summaries/summary/46179/symptom-management-in-patients-with-lung-cancer-diagnosis-and-management-of-lung-cancer-3rd-ed-american-college-of-chest-physicians-evidencebased-clinical-practice-guidelines"
+            }
+        ],
+        "classification": [
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/cited-artifact-classification-type",
+                            "code": "keyword"
+                        }
+                    ]
+                },
+                "classifier": [
+                    {
+                        "text": "acetaminophen"
+                    },
+                    {
+                        "text": "adaptation, psychological"
+                    }
+             
+                ],
+                "whoClassified": {
+                    "publisher": {
+                        "reference": "Organization/ngc",
+                        "display": "NGC"
+                    }
+                }
+            },
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/cited-artifact-classification-type",
+                            "code": "keyword"
+                        }
+                    ]
+                },
+                "classifier": [
+                    {
+                        "coding": [
+                            {
+                                "system": "http://www.nlm.nih.gov/research/umls/rxnorm",
+                                "code": "161",
+                                "display": "acetaminophen"
+                            },
+                            {
+                                "system": "http://snomed.info/sct",
+                                "code": "387517004",
+                                "display": "Acetaminophen"
+                            }
+                        ],
+                        "text": "acetaminophen"
+                    }
+                ],
+                "whoClassified": {
+                    "publisher": {
+                        "display": "AHRQ CEDAR"
+                    }
+                }
+            },
+            {
+                "type": {
+                    "coding": [
+                        {
+                            "system": "http://terminology.hl7.org/CodeSystem/cited-artifact-classification-type",
+                            "code": "knowledge-artifact-type"
+                        }
+                    ]
+                },
+                "classifier": [
+                    {
+                        "text": "Guideline"
+                    }
+                ]
+            }
+        ]
+    },
+   "resourceType": "Citation"
+}
+
+export default searchResult;
