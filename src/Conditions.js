9e114fd4e124b5decde2d0e19fc31ee25bc95d5a
Marc Hadley <mhadley@mitre.org>
2021-10-20 16:01:03 -0400

    Add extra code system display names

--- src/Conditions.js
+++ src/Conditions.js
@@ -7,7 +7,13 @@ function Conditions({ conditions, onChange }) {
   const [selectedConditionIds, setSelectedConditionIds] = useState([]);
 
   const CODE_SYSTEMS = {
-    'http://snomed.info/sct': 'SNOMED-CT'
+    'http://snomed.info/sct': 'SNOMED-CT',
+    'https://www.nlm.nih.gov/mesh/': 'MeSH',
+    'http://www.nlm.nih.gov/research/umls/medlineplus': 'Medline Plus',
+    'http://snomed.info/sct/449081005': 'SNOMED-CT (ESP)',
+    'http://www.nlm.nih.gov/research/umls/mshspa': 'MeSH (ESP)',
+    'http://hl7.org/fhir/sid/icd-10-cm': 'ICD-10-CM',
+    'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm'
   }
 
   const getCodeSystemName = (codeSystemUrl) => {
