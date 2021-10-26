0d27c5f4256295a8db74592826dd0c5de03117af
Diana Eastman <deastman@mitre.org>
2021-10-26 19:51:52 -0400

    Add constants.js with one constant: CODE_SYSTEMS.

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+module.exports = Object.freeze({
+	
+	CODE_SYSTEMS: Object.freeze({
+	    'http://snomed.info/sct': 'SNOMED-CT',
+	    'https://www.nlm.nih.gov/mesh/': 'MeSH',
+	    'http://www.nlm.nih.gov/research/umls/medlineplus': 'Medline Plus',
+	    'http://snomed.info/sct/449081005': 'SNOMED-CT (ESP)',
+	    'http://www.nlm.nih.gov/research/umls/mshspa': 'MeSH (ESP)',
+	    'http://hl7.org/fhir/sid/icd-10-cm': 'ICD-10-CM',
+	    'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm',
+  	})
+})
