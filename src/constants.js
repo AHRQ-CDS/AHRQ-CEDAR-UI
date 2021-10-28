f536fa8ec51775afe17cff768715a725d25f7f67
Marc Hadley <mhadley@mitre.org>
2021-10-28 17:42:57 -0400

    Change MeSH URI to match the one used by cedar_api

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 module.exports = Object.freeze({
-	
+
 	CODE_SYSTEMS: Object.freeze({
 	    'http://snomed.info/sct': 'SNOMED-CT',
-	    'https://www.nlm.nih.gov/mesh/': 'MeSH',
+	    'http://terminology.hl7.org/CodeSystem/MSH': 'MeSH',
 	    'http://www.nlm.nih.gov/research/umls/medlineplus': 'Medline Plus',
 	    'http://snomed.info/sct/449081005': 'SNOMED-CT (ESP)',
 	    'http://www.nlm.nih.gov/research/umls/mshspa': 'MeSH (ESP)',
