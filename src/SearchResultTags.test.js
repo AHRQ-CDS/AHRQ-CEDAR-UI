5a968776dd007ec73584223965c1e1f08e4d2a94
Diana Eastman <deastman@mitre.org>
2021-10-28 09:46:39 -0400

    Remove errant semi-colon.

--- src/SearchResultTags.test.js
+++ src/SearchResultTags.test.js
@@ -4,7 +4,7 @@ import { mount } from 'enzyme';
 import concepts from '../fixtures/concepts';
 
 const keywords = ['acute disease', 'acute myocardial infarction', 'arrhythmia, sinus', 'arrhythmias, cardiac']
-const onKeywordClick = () => void 0;;
+const onKeywordClick = () => void 0;
 
 it('renders without crashing', () => {
   mount(<SearchResultTags keywords={keywords} concepts={concepts} onKeywordClick={onKeywordClick}/>);
