2a6fd9e7f8f9898003cb0f6d5070648b5971d1c5
Diana Eastman <deastman@mitre.org>
2021-11-18 16:16:06 -0500

    Add selectedConcepts as dependency.

--- src/Conditions.test.js
+++ src/Conditions.test.js
@@ -3,8 +3,6 @@ import Conditons from './Conditions';
 import { mount } from 'enzyme';
 import conditons from '../fixtures/conditions';
 
-const conceptIsSelected = (concept) => void 0;
-
 it('renders without crashing', () => {
-  mount(<Conditons conditions={conditons} conceptIsSelected={conceptIsSelected} />);
+  mount(<Conditons conditions={conditons} selectedConcepts={[]} />);
 });
