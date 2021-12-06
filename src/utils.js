1d6fbe4d9d9a5c3348b8014ca6ff7cfe52e5ab11
Diana Eastman <deastman@mitre.org>
2021-12-06 15:59:08 -0500

    Add class for dealing with base64 conversion.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import {compressToBase64, decompressFromBase64} from 'lz-string';
+
 export const conceptIsSelected = (conceptToTest, selectedConcepts) => {
   const conceptCodesToTest = conceptToTest.coding.map(code => `${code.code}|${code?.system}`);
   const selectedConceptCodes = selectedConcepts.map(condition => condition.coding.map(code => `${code.code}|${code?.system}`));
@@ -9,4 +11,38 @@ export const conceptIsSelected = (conceptToTest, selectedConcepts) => {
   }
 
   return false;
+}
+
+export default class urlSearchObject {
+  constructor(selectedKeywords, selectedConcepts, searchString, searchPage, searchPublisher, searchStatus, searchParameter, lastUpdatedSearchString, lastUpdatedPreset) {
+    this.selectedKeywords = selectedKeywords;
+    this.selectedConcepts = selectedConcepts;
+    this.searchString = searchString;
+    this.searchPage = searchPage;
+    this.searchPublisher = searchPublisher;
+    this.searchStatus = searchStatus;
+    this.searchParameter = searchParameter;
+    this.lastUpdatedSearchString = lastUpdatedSearchString;
+    this.lastUpdatedPreset = lastUpdatedPreset;
+  }
+
+  getAsBase64() {
+    const urlSearchObj = {
+      selectedKeywords: this.selectedKeywords,
+      selectedConcepts: this.selectedConcepts,
+      searchString: this.searchString,
+      searchPage: this.searchPage,
+      searchPublisher: this.searchPublisher,
+      searchStatus: this.searchStatus,
+      searchParameter: this.searchParameter,
+      lastUpdatedSearchString: this.lastUpdatedPreset === 'Custom' ? this.lastUpdatedSearchString : '',
+      lastUpdatedPreset: this.lastUpdatedPreset
+    }
+
+    return compressToBase64(JSON.stringify(urlSearchObj));
+  }
+
+  static convertFromBase64(base64SearchString) {
+    return JSON.parse(decompressFromBase64(base64SearchString));
+  }
 }
