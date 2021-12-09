8d162042b3bc1dc3c3d993073ecd977465dae25b
Diana Eastman <deastman@mitre.org>
2021-12-09 17:55:54 -0500

    Rename previouslySelectedConceptCodes --> selectedConceptCodes.

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 export const conceptIsSelected = (conceptToTest, selectedConcepts) => {
   const conceptCodesToTest = conceptToTest.coding.map(code => `${code.code}|${code?.system}`);
-  const previouslySelectedConceptCodes = selectedConcepts.map(condition => condition.coding.map(code => `${code.code}|${code?.system}`));
+  const selectedConceptCodes = selectedConcepts.map(condition => condition.coding.map(code => `${code.code}|${code?.system}`));
 
-  for(const concept of previouslySelectedConceptCodes) {
+  for(const concept of selectedConceptCodes) {
     if(conceptCodesToTest.every(c=> concept.includes(c))) {
       return true;
     } 
