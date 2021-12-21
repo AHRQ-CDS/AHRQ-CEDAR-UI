0239ef5679ba272d895e91e010f2ebf81292c6fc
Marc Hadley <mhadley@mitre.org>
2021-12-21 15:45:15 -0500

    Add UMLS MTH as a supported code system for UMLS CUIs

--- src/constants.js
+++ src/constants.js
@@ -1,11 +1,12 @@
 module.exports = Object.freeze({
-	CODE_SYSTEMS: Object.freeze({
-		'http://hl7.org/fhir/sid/icd-10-cm': 'ICD-10-CM',
-		'http://www.nlm.nih.gov/research/umls/medlineplus': 'Medline Plus',
-		'http://terminology.hl7.org/CodeSystem/MSH': 'MeSH',
-		'http://www.nlm.nih.gov/research/umls/mshspa': 'MeSH (ESP)',
-	    'http://snomed.info/sct': 'SNOMED-CT',
-	    'http://snomed.info/sct/449081005': 'SNOMED-CT (ESP)',
-	    'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm',
-  	})
+  CODE_SYSTEMS: Object.freeze({
+    'http://www.nlm.nih.gov/research/umls/mth': 'UMLS MTH',
+    'http://hl7.org/fhir/sid/icd-10-cm': 'ICD-10-CM',
+    'http://www.nlm.nih.gov/research/umls/medlineplus': 'Medline Plus',
+    'http://terminology.hl7.org/CodeSystem/MSH': 'MeSH',
+    'http://www.nlm.nih.gov/research/umls/mshspa': 'MeSH (ESP)',
+    'http://snomed.info/sct': 'SNOMED-CT',
+    'http://snomed.info/sct/449081005': 'SNOMED-CT (ESP)',
+    'http://www.nlm.nih.gov/research/umls/rxnorm': 'RxNorm',
+  })
 })
