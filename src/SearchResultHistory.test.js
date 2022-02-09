c054d2af349bfac59b81fa34963d01192f2834d3
Diana Eastman <deastman@mitre.org>
2022-02-08 19:01:27 -0500

    Fix spacing in SearchResultHistory test.

--- src/SearchResultHistory.test.js
+++ src/SearchResultHistory.test.js
@@ -3,8 +3,8 @@ import SearchResultHistory from './SearchResultHistory';
 import { mount } from 'enzyme';
 import searchResult from '../fixtures/searchResult';
 
-const leftResource = { searchResult}
-const rightResource = { searchResult}
+const leftResource = searchResult;
+const rightResource = searchResult;
 
 it('renders without crashing', () => {
   mount(<SearchResultHistory leftResource={leftResource} rightResource={rightResource} />);
