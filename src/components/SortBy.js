05dbe497739076de44c492e3c0859ca8d7c7ff3e
Adam Barnes <ajbarnes@mitre.org>
2022-04-04 20:39:52 -0400

    feat: convert sort radio to dropdown + plus implement feedback

--- src/components/SortBy.js
+++ src/components/SortBy.js
@@ -1,69 +1,60 @@
 import React from 'react';
-import { Form, Popup, Icon } from 'semantic-ui-react';
+import { Dropdown, Popup } from 'semantic-ui-react';
 import { SORT_BY_OPTIONS } from '../utils/constants';
 
 function SortBy({sortByPreset, setSortByPreset}) {
+  const humanReadableOptions = Object.keys(SORT_BY_OPTIONS)
 
   const handleSortByChange = (event) => {
     const {target} = event;
-    const sortByPreset = target.value;
-    setSortByPreset(sortByPreset);
+    const selection = target.innerText;
+
+    setSortByPreset(selection);
+  }
+
+  const createOptions = (options) => {
+    return options.map((option) => {
+      if (option === 'Default') {
+        return (
+          <Popup
+            key={option}
+            // Dropdown doesn't register Popup nested Item as onChange, handle w/ onClick instead
+            trigger={<Dropdown.Item text={option} onClick={handleSortByChange}/>}
+            position='right center'
+            wide
+          >
+            <p>The default sorting, in this order, sorts by:</p>
+            <ol>
+              <li>Status (active to retracted)</li>
+              <li>Publish date (most to least recent)</li>
+              <li>Strength of recommendation</li>
+              <li>Quality of evidence</li>
+            </ol>
+            <p>
+              If concepts are included in a search, results also sort 
+              by number of concept matches.
+            </p>
+          </Popup>
+        )
+      }
+      else {
+        return <Dropdown.Item key={option} text={option} />
+      }
+    })
   }
 
   return (
-    <> 
+    <>
       <h4>Sort By</h4>
-      <Form>
-        <Form.Group grouped>
-          {/* "Default" sort gets an explanation popup, followed by other options */}
-          {SORT_BY_OPTIONS.map((option, idx) => {
-            if (idx === 0) {
-              return (
-                <>
-                  <Form.Field
-                    control='input'
-                    type='radio'
-                    name='sortByRadio'
-                    className='normal-weight default-sort'
-                    label={option}
-                    key={option}
-                    value={option}
-                    onChange={handleSortByChange}
-                    checked={sortByPreset === option }
-                  />
-                  <Popup trigger={<Icon name='question circle outline' color='blue' size='small' />}
-                         position='right center'
-                         wide
-                  >
-                    <p>The default sorting, in this order, sorts by:</p>
-                    <ol>
-                      <li>Status (active to retracted)</li>
-                      <li>Publish date (most to least recent)</li>
-                      <li>Strength of recommendation</li>
-                      <li>Quality of evidence</li>
-                    </ol>
-                    <p>
-                      If concepts are included in a search, results also sort 
-                      by number of concept matches.
-                    </p>
-                  </Popup>
-                </>
-              )
-            }
-            return <Form.Field
-              control='input'
-              type='radio'
-              name='sortByRadio'
-              className='normal-weight'
-              label={option}
-              key={option}
-              value={option}
-              onChange={handleSortByChange}
-              checked={sortByPreset === option }
-            />
-          })}
-        </Form.Group>
-      </Form>
+      <Dropdown
+        text={sortByPreset}
+        name="sortByOptions"
+        fluid
+        selection
+        onChange={handleSortByChange}
+        options={createOptions(humanReadableOptions)}
+        value={sortByPreset}
+      />
     </>
   )
 }
